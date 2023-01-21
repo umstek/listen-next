355c7a267a8b2ae26b3080aeb222410ec263f7f2
Wickramaranga Abeygunawardhana <umstek@live.com>
2023-01-21 20:52:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# vite-typescript-react-tailwind
+
+## What we have here
+
+1. Vite
+2. TypeScript
+3. ReactJS
+4. TailwindCSS
+5. PostCSS
+6. ESLint
+7. Prettier
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div id="app">
+      <h1 className="bg-blue-50 text-blue-600 font-sans font-thin text-4xl">
+        Hello, world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
