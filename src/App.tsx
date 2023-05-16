65cb05757a0f0b5ff371bb6cbe8fa9436b2738d9
Wickramaranga Abeygunawardhana <umstek@live.com>
2023-05-16 21:37:03 +0530

    feat(player): :sparkles: add player-core functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import { Player } from './components/Player';
+
 function App() {
   return (
     <div id="app">
-      <h1 className="bg-blue-50 text-blue-600 font-sans font-thin text-4xl">
-        Hello, world!
-      </h1>
+      <Player />
     </div>
   );
 }
